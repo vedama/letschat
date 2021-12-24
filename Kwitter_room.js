5f2b13d7078b8c5b5739f4324b90758ea0b121d9
Arihanth Vedam - Whitehat Jr. Student <96354653+vedama@users.noreply.github.com>
2021-12-23 19:02:33 -0500

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDYew783WFSf8mS_iipxAWp6HKQWo70YI",
+    authDomain: "let-s-chat-36a95.firebaseapp.com",
+    databaseURL: "https://let-s-chat-36a95-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-36a95",
+    storageBucket: "let-s-chat-36a95.appspot.com",
+    messagingSenderId: "751689926892",
+    appId: "1:751689926892:web:13f2dff33520c97af83dc9"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
